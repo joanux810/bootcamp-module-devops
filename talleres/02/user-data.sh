f7c627c5c55878a391596064960fa6a89f0bc596
EC2 Default User <ec2-user@ip-172-31-56-138.ec2.internal>
2020-07-10 00:44:55 +0000

    Added user data in the yaml file

--- talleres/02/user-data.sh
+++ talleres/02/user-data.sh
@@ -1,9 +1,10 @@
-#!/bin/bash -xe
-
-yum install -y python37
-curl -O https://bootstrap.pypa.io/get-pip.py
-python3 get-pip.py
-git clone https://github.com/eloyvega/bootcamp-devops.git
-cd ~/bootcamp-devops/backend/
-pip install -r requirements.txt
-nohup gunicorn -w 3 -b 0.0.0.0:8000 app:app &
+ #!/bin/bash -xe
+  exec > >(tee /var/log/cloud-init-output.log|logger -t user-data -s 2>/dev/console) 2>&1
+  yum install -y python37 git
+  curl -O https://bootstrap.pypa.io/get-pip.py
+  python3 get-pip.py
+  cd ~
+  git clone https://github.com/joanux810/bootcamp-module-devops.git
+  cd ~/bootcamp-module-devops/backend/
+  pip install -r requirements.txt
+  nohup gunicorn -w 3 -b 0.0.0.0:8000 app:app &
